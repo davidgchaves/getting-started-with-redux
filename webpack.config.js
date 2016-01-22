871e3d5c0d511b5a5156bb99c821cb85ed7d2003
David G Chaves <davidgchaves@gmail.com>
2016-01-22 09:21:03 +0100

    Add config files from my react boilerplate project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack'),
+    path    = require('path');
+
+var config = {
+  entry: [
+    './front/main.js',
+    './front/index.html',
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:3000'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]"
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
+
+module.exports = config;
