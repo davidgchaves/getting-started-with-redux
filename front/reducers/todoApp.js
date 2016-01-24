e8243bfd45ddb358f97827f6d71290fece6346a9
David G Chaves <davidgchaves@gmail.com>
2016-01-24 15:44:39 +0100

    Fix imports of todoApp reducer

--- front/reducers/todoApp.js
+++ front/reducers/todoApp.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import todos from 'reducers/todos';
-import visibilityFilter from 'reducers/visibilityFilter';
+import todos from './todos';
+import visibilityFilter from './visibilityFilter';
 
 const todoApp = combineReducers({
   todos,
