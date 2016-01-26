67e48c17f2d5e39cdd19a4583c5e342bd7da50d9
David G Chaves <davidgchaves@gmail.com>
2016-01-26 13:52:57 +0100

    Simplify initial render

--- front/main.jsx
+++ front/main.jsx
@@ -1,14 +1,8 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
-import store   from './store';
 import TodoApp from './components/TodoApp.jsx';
 
-const render = () => {
-  ReactDOM.render(
-    <TodoApp {...store.getState()} />,
-    document.getElementById('todo-app')
-  );
-};
-
-store.subscribe(render);
-render();
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('todo-app')
+);
