96ad629e3826256e21634a52261173b536bec25d
David G Chaves <davidgchaves@gmail.com>
2016-01-26 13:40:25 +0100

    CONTAINER Components: VisibleTodoList

--- front/components/TodoApp.jsx
+++ front/components/TodoApp.jsx
@@ -122,6 +122,33 @@ const getVisibleTodos = (todos, filter) => {
   }
 };
 
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => this.forceUpdate());
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const props = this.props;
+    const state = store.getState();
+
+    return (
+      <TodoList
+        todos={getVisibleTodos(state.todos, state.visibilityFilter)}
+        onTodoClick={id =>
+          store.dispatch({
+            type: 'TOGGLE_TODO',
+            id
+          })
+        }
+      />
+    );
+  }
+}
+
 let nextTodoId = 0;
 const TodoApp = ({ todos, visibilityFilter }) => (
   <div>
@@ -134,16 +161,8 @@ const TodoApp = ({ todos, visibilityFilter }) => (
         })
       }
     />
-  <TodoList
-    todos={getVisibleTodos(todos, visibilityFilter)}
-    onTodoClick={id =>
-      store.dispatch({
-        type: 'TOGGLE_TODO',
-        id
-      })
-    }
-  />
-  <Footer />
+    <VisibleTodoList />
+    <Footer />
   </div>
 );
 /*
