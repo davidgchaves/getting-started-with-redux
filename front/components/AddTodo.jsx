9d6129db689ae010fac06e3f9197c48e6d8567c6
David G Chaves <davidgchaves@gmail.com>
2016-01-27 10:24:36 +0100

    Refactor AddTodo to leverage connect (React-Redux)

--- front/components/AddTodo.jsx
+++ front/components/AddTodo.jsx
@@ -1,16 +1,16 @@
 import React        from 'react';
-import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
 
 let nextTodoId = 0;
 
-const AddTodo = (props, { store }) => {
+let AddTodo = ({ dispatch }) => {
   let input;
 
   return (
     <div>
       <input ref={node => { input = node; }}></input>
       <button onClick={() => {
-        store.dispatch({
+        dispatch({
           type: 'ADD_TODO',
           id: nextTodoId++,
           text: input.value
@@ -22,8 +22,38 @@ const AddTodo = (props, { store }) => {
     </div>
   );
 };
-AddTodo.contextTypes = {
-  store: React.PropTypes.object
-};
+
+/*
+ * const mapStateToProps = (state) => {
+ *   return {};
+ * };
+ *
+ * const mapDispatchToProps = (dispatch) => {
+ *   return { dispatch };
+ * };
+ *
+ * AddTodo = connect(
+ *   mapStateToProps,
+ *   mapDispatchToProps
+ * )(AddTodo);
+ *
+ * It is wasteful to even subscribe to the Redux Store
+ * if we don't calculate any Props from its state:
+ *   - Replace mapStateToProps with a null
+ *     (there is no need to subscribe to the Redux Store)
+ * It is a common pattern to just inject the 'dispatch' function,
+ * and 'connect' is aware of this fact, you just need to:
+ *   - Replace mapDispatchToProps with a null
+ *
+ * AddTodo = connect(
+ *   null,
+ *   null
+ * )(AddTodo);
+ *
+ * Or even better, leverage the default behaviour of 'connect':
+ *   - Do not subscribe to the Redux Store
+ *   - Inject only the 'dispatch' function as a Prop
+ */
+AddTodo = connect()(AddTodo);
 
 export default AddTodo;
